166494a943e3ec1029edb36e371a39a852cf5d75
shamamamber <samber.msis22seecs@seecs.edu.pk>
2024-08-19 17:25:07 +0500

    New Added

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My First Deployment',
+      title: 'My First Deploymentt',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
